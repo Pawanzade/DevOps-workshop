e8b3ecd9b382181e52a50c0e3f407bdd5cdf3bea
Pawan <pwnshamzade@gmail.com>
2024-01-02 11:35:32 +0530

    for new jenkins master

--- terraform_code/V3-EC2_with_VPC.tf
+++ terraform_code/V3-EC2_with_VPC.tf
@@ -37,6 +37,15 @@ resource "aws_security_group" "demo-sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "Jenkins Port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
